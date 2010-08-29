1eee512a67289ee070700cf37a8a7ce77d940480
Cal Henderson <cal@iamcal.com>
2010-08-29 17:04:26 -0500

    initial version from TS

---
+++ server.js
@@ -0,0 +1,187 @@
+var sys = require('sys');
+var http = require('http');
+var url = require('url');
+var cp = require('child_process');
+
+var config = {
+	files : [
+		'/var/log/messages',
+	],
+	max_lines : 100,
+	send_on_fresh : 20,
+	idle_timeout: 10000, // 1s
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function logTailer(){
+
+	var self = this;
+
+	this.buffer = "";
+	this.first_line = 1;
+	this.next_line = 1;
+	this.lines = {};
+
+
+	//
+	// launch the log tailer
+	//
+
+	var args = ['-F'];
+	for (var i=0; i<config.files.length; i++) args.push(config.files[i]);
+
+	this.proc = cp.spawn('tail', args);
+
+	this.proc.stderr.on('data', function(data){
+		console.log('ERROR '+data);
+	});
+	this.proc.stdout.on('data', function(data){
+
+		self.buffer += data.toString('utf8');
+		self.consumeBuffer();
+	});
+	this.proc.on('exit', function(code){
+		console.log('tailing process exited');
+	});
+
+
+	//
+	// try and extract lines from `buffer` and stash them
+	// in the lines hash.
+	//
+
+	this.consumeBuffer = function(){
+
+		var sent = 0;
+
+		while (1){
+			var idx = self.buffer.indexOf("\n");
+			if (idx == -1){
+				if (sent){
+					self.cleanup();
+					self.emit('lines');
+				}
+				return;
+			}
+
+			var line = self.buffer.substr(0, idx);
+			self.buffer = self.buffer.substr(idx+1);
+
+			self.lines[self.next_line] = line;
+			//console.log('added line '+self.next_line);
+			self.next_line++;
+			sent++;
+			self.emit('line', self.next_line-1);
+		}
+	}
+
+	this.cleanup = function(){
+
+		// scrub old lines here
+
+		var min_start = self.next_line - config.max_lines;
+		if (self.first_line < min_start){
+			//console.log('pruning lines from '+self.first_line+' to '+min_start);
+			for (var i=self.first_line; i<min_start; i++){
+				delete self.lines[i];
+			}
+			self.first_line = min_start;
+		}
+	}
+
+
+	//
+	// get the index to start reading from (for a new connection)
+	//
+
+	this.getLines = function(start){
+
+		var lines = [];
+
+		if (!start){
+			start = self.next_line - config.send_on_fresh;
+			if (start < self.first_line) start = self.first_line;
+		}
+
+		// are they asking to start before we have logs?
+		if (start < self.first_line){
+			lines.push('[skipping forward]');
+			start = self.first_line;
+		}
+
+		// are they asking for stuff from the future?
+		if (start > self.next_line){
+			lines.push('[log server restarted]');
+			start = self.first_line;
+		}
+
+		// are they asking for what's about to come?
+		if (start == self.next_line){
+			return [];
+		}
+
+		// they must be asking for lines we have
+		for (var i=start; i<self.next_line; i++){
+			lines.push(self.lines[i]);
+		}
+
+		return lines;
+	}
+}
+
+sys.inherits(logTailer, process.EventEmitter);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function logServer(tailer){
+
+	http.createServer(function (req, res){
+
+		res.writeHead(200, {
+			'Content-Type': 'text/plain',
+			'Access-Control-Allow-Origin': '*',  
+		});
+
+		var _url = url.parse(req.url, true);
+		var get = _url.query || {};
+
+		var lines = tailer.getLines(parseInt(get.l));
+		if (lines.length){
+			// return right away - we have lines!
+			res.end(JSON.stringify({ok: 1, lines: lines, l: tailer.next_line}));
+			return;
+		}
+
+		var new_idx = tailer.next_line;
+		var ended = false;
+		var listener = null;
+		var timer = setTimeout(function(){
+			if (ended) return;
+			ended = true;
+			res.end(JSON.stringify({ok :1, timeout: 1}));
+			tailer.removeListener('lines', listener);
+
+		}, config.idle_timeout);
+
+		listener = function(){
+			if (ended) return;
+			ended = true;
+			lines = tailer.getLines(new_idx);
+			res.end(JSON.stringify({ok: 1, lines: lines, l: tailer.next_line}));
+			clearTimeout(timer);
+		};
+
+		tailer.on('lines', listener);
+
+	}).listen(8124);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var tailer = new logTailer();
+var server = new logServer(tailer);
+
